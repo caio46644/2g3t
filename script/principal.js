7264fe3bb8f58f0472f09e1386b2ab382fb247ad
caio46644 <108937026+caio46644@users.noreply.github.com>
2023-11-20 11:44:32 -0300

    20/11

--- script/principal.js
+++ script/principal.js
@@ -7,13 +7,21 @@ raiz = a => Math.sqrt(a);
 eq2grau = (a,b,c) => {
     if(a == 0) return "Não é uma equação do Segundo Grau.";
     let delta = sub(mult(b,b),mult(4,mult(a,c)));
+
+    document.getElementById("delta").innerHTML = delta;
+
     if(delta < 0) return "Não Possui raiz real.";
     if(delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
     return "x1 " + div(soma(-b,raiz(delta)),mult(2,a)) + 
            "x2 " + div(sub(-b,raiz(delta)),mult(2,a));
 }
-let a = "";
-let b = "";
+let a = "0";
+let b = "0";
+let c = "0";
+let sa = "+";
+let sb = "+";
+let sc = "+";
+
 let op = "";
 let valor = "";
 let tem_ponto = false;
@@ -91,4 +99,76 @@ function digitando(tecla){
     }
     valor = valor + tecla;
     mostra_resultado(valor);
+}
+const set_sinal_a = () =>{
+    sa = document.getElementById("sinal_a").value;
+    a = Number(a);
+    a = -a;
+    calcular2g();
+}
+const set_sinal_b = () =>{
+    sb = document.getElementById("sinal_b").value;
+    b = Number(b);
+    b = -b;
+    calcular2g();
+}
+const set_sinal_c = () =>{
+    sc = document.getElementById("sinal_c").value;
+    c = Number(c);
+    c = -c;
+    calcular2g();
+}
+const set_valor_a = () =>{
+    a = document.getElementById("valor_a").value;
+    a = Number(a);
+    if(sa == "-") a = -a;
+    calcular2g();
+}
+const set_valor_b = () =>{
+    b = document.getElementById("valor_b").value;
+    b = Number(b);
+    if(sb == "-") b = -b;
+    calcular2g();
+}
+const set_valor_c = () =>{
+    c = document.getElementById("valor_c").value;
+    c = Number(c);
+    if(sc == "-") c = -c;
+    calcular2g();
+}
+const calcular2g = ()=>{
+    document.getElementById("raiz").innerHTML = eq2grau(a,b,c);
+}
+const pa_al = "";
+const pa_n = "";
+const pa_r = "";
+const mostrar_pa = () =>{
+    if(pa_a1 != "" && pa_n != "" && pa_r != ""){
+        let pa = pa_a1;
+        let ult_termo = 1;
+        for(let i=1; i < pa_n; i++){
+            if(ult_termo < 4){
+                pa = ", " + (Number(pa_a1) + (i*pa_r) );
+                ult_termo++;
+
+                }
+            }
+            if(ult_termo > 3){
+                pa = ", ... , " + (Number(pa_a1) + (pa_n - 1) * r);
+            }
+            document.getElementById("pa_seq").innerHTML = pa;
+        }
+    }
+
+const setpa_a1 = () => {
+    pa_a1 = document.getElementById("pa_a1").value;
+    mostrar_pa();
+}
+const setpa_n = () => {
+    pa_n = document.getElementById("pa_n").value;
+    mostrar_pa();
+}
+const setpa_r = () => {
+    pa_r = document.getElementById("pa_r").value;
+    mostrar_pa();
 }
